d95c320a7fd71e61e94698e6d720d382f66f94ca
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-16 02:17:02 +0530

    Linking

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,15 @@ connectCloudinary()
 //middleware configuration
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use(
+    cors({
+        origin: ["https://shipshopstorefrontend.vercel.app"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 //---------------------------------------api endpoints-------------------------------------
 //userController
