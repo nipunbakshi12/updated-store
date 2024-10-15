94b5cbeeffd10108c9dbc15fc3b28095b4378bc6
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-16 02:19:34 +0530

    Linking1

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./components/Login.jsx";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://shipshopstorebackend.vercel.app'
 export const currency = '₹'
 
 const App = () => {
