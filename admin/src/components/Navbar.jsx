853f62ecdd9164e62e45a9ff0d220b4f81fecfd7
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-15 21:48:20 +0530

    Now Finalised

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../../frontend/src/assets/assets'
 const Navbar = ({ setToken }) => {
   return (<>
     <div className='flex justify-between items-center py-2 px-[4%]'>
-      <img className='w-[max(10%,80px)]' src={assets.logo} />
+      <img className='w-[max(10%,80px)]' src={assets.logo3} />
       <button onClick={() => setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>
         Log-Out
       </button>
