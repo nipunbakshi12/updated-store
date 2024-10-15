39355a55173eaebe4ad7a73562b252e53b2d0cf5
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-16 02:34:54 +0530

    dotenvkichecking

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+// import 'dotenv/config'
 import connectCloudinary from './config/cloudinary.js';
 import connectDB from './config/db.js';
 import userRouter from './routes/userRoute.js';
