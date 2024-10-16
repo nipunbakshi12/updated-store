700ad80c7af1983399ba4f774c8cc6aa4db64add
nipunbakshi12 <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-16 13:13:47 +0530

    Update App.jsx mein rerender ka backend url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from "./components/Login.jsx";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = 'http://localhost:4000'
+export const backendUrl = 'https://updated-store-admin.onrender.com'
 export const currency = '₹'
 
 const App = () => {
