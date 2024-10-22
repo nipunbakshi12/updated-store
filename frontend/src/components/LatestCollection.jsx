7017531d8ac3e019bc3b4d3d6a02610663f09a11
Nipun Bakshi <156699344+nipunbakshi12@users.noreply.github.com>
2024-10-22 22:43:34 +0530

    Update LatestCollection.jsx added Loader

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -23,11 +23,24 @@ const LatestCollection = () => {
             </div>
 
             {/* RENDERING PRODUCTS */}
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+
+            {
+                latestProducts ?
+                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                        {latestProducts.map((item, index) => (
+                            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                        ))}
+                    </div> :
+                    <div>
+                        <h1>Loading...</h1>
+                    </div>
+
+            }
+{/*             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {latestProducts.map((item, index) => (
                     <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
                 ))}
-            </div>
+            </div> */}
 
 
         </div>
